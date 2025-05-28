dd301f73202c1c17a151a4deea8f5d5029790444
Tomas-Arsal <tomas.arsal.23@gmail.com>
2025-05-28 09:46:58 +0300

    finishing localization

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -17,7 +17,6 @@ import 'package:intl/message_lookup_by_library.dart';
 import 'package:intl/src/intl_helpers.dart';
 
 import 'messages_ar.dart' as messages_ar;
-import 'messages_en.dart' as messages_en;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
@@ -29,8 +28,6 @@ MessageLookupByLibrary? _findExact(String localeName) {
   switch (localeName) {
     case 'ar':
       return messages_ar.messages;
-    case 'en':
-      return messages_en.messages;
     default:
       return null;
   }
